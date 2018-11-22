e7d0563dafcfe828584f8771fb25a2cf1b2ed274
JcSleiman <jcsleiman17@gmail.com>
2018-11-22 15:19:24 +0000

    Practica CRUD Nginx, php, mysql

--- AdmServ/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'root';
-$databaseUsername = 'tiendita';
-$databasePassword = './Metalhalo1600';
-
-$mysqli = mysqli_connect($databaseHost, $databaseName, $databasePassword, $databaseUsername);
-?>
